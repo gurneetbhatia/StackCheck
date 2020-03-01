f1e4a3ea1cafa94bf23ec723951b8c597253ae80
Gurneet Bhatia <36604284+needabath@users.noreply.github.com>
2020-03-01 07:52:39 +0000

    partial upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace oof1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "JAVA",
+            "C++",
+            "C#",
+            "C",
+            "Python",
+            "PHP",
+            "Pascal",
+            "HTML",
+            "CSS"});
+            this.comboBox1.Location = new System.Drawing.Point(47, 17);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(226, 34);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.Text = "Coding Language";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(380, 324);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "MAKE MY CODE WHOLESOME";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(47, 100);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(860, 209);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Enter your code";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(182, 365);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(203, 33);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(42, 368);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 26);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Your score:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(44, 425);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(128, 26);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Good Code";
+            // 
+            // textBox3
+            // 
+            this.textBox3.AcceptsReturn = true;
+            this.textBox3.AcceptsTab = true;
+            this.textBox3.AllowDrop = true;
+            this.textBox3.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(47, 471);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(425, 260);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(503, 425);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 26);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Bad Code";
+            // 
+            // textBox4
+            // 
+            this.textBox4.AcceptsReturn = true;
+            this.textBox4.AcceptsTab = true;
+            this.textBox4.AllowDrop = true;
+            this.textBox4.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.Location = new System.Drawing.Point(493, 471);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox4.Size = new System.Drawing.Size(414, 260);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("MS Reference Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(698, 19);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(209, 33);
+            this.textBox5.TabIndex = 11;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(961, 613);
+            //this.Controls.Add(this.label5);
+            //this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "WHOLESOME CODE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Globalization;
+using Algorithmia;
+//using AlgorithmiaPipe;
+using CsvHelper;
+using Microsoft.VisualBasic.FileIO;
+using Newtonsoft.Json.Linq;
+
+namespace oof1
+{
+    public partial class Form1 : Form
+    {
+        private string input = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            //String path = @"C:\Users\greta_000\OneDrive\hackaton Broah\oof1\InputText.txt";
+            //File.WriteAllText(path, textBox1.Text);
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input = textBox1.Text;
+            string code = convert(input);
+
+            List<string>[] output = MainAlgorithm.Run(code);
+            List<string> goodAnswers = output[0];
+            List<string> badAnswers = output[1];
+            List<string> extraData = output[2];
+            float score = float.Parse(extraData.ElementAt(0));
+            float total = float.Parse(extraData.ElementAt(1));
+            textBox2.Text = (score * 100 / total) + "%";
+            string goodAnsOut = "";
+            for(int i = 0; i < goodAnswers.Count; i++)
+            {
+                goodAnsOut += goodAnswers.ElementAt(i) + "\n\n";
+            }
+            textBox3.Text = goodAnsOut;
+            string badAnsOut = "";
+            for (int i = 0; i < badAnswers.Count; i++)
+            {
+                badAnsOut += badAnswers.ElementAt(i)+"\n\n";
+            }
+            textBox4.Text = badAnsOut;
+
+            
+
+        }
+
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            //String path = @"C:\Users\greta_000\OneDrive\hackaton Broah\oof1\InputText.txt";
+            //File.Create(path);
+        }
+
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private string convert(string input)
+        {
+            string code = "";
+            using (StringReader reader = new StringReader(input))
+            {
+                string line;
+                string line1 = "";
+                string[] words = { };
+                line = reader.ReadLine();
+                while (line != null)
+                {
+                    words = line.Split(' ');
+                    for (int j = 0; j < words.Length; j++)
+                    {
+                        if (j < words.Length - 1 && words[j + 1].Contains("="))
+                            line1 = line1 + " * ";
+                        else
+                            line1 = line1 + words[j] + " ";
+                    }
+                    code = code + line1 + "\n";
+                    line1 = "";
+                    line = reader.ReadLine();
+                }
+            }
+            return code;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    static class MainAlgorithm
+    {
+        //static List data;
+        public static List<string>[] Run(string input)
+        {
+            /*var input = "{"
+             + "  \"document\": \"This is horrible code. It looks like it was written by a MMU student. I would not trust you to solve a kindergarten maths problem.\""
+             + "}";
+            JArray result = (Newtonsoft.Json.Linq.JArray)getSentiment(input).result;
+            Console.WriteLine((float)result[0]["sentiment"]);
+
+            using (var reader = new StreamReader("/Users/inderdeepbhatia/Projects/HTBTest1/HTBTest1/data_please.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<dynamic>();
+                data = records.ToArray();
+            }
+            var codeInput = "'Object obj = new Object();'";
+            var codeCSV = "'Object ob = new Object();'";
+            var matchRatio = "python3 sequence.py " + codeInput + " " + codeCSV;
+            Console.WriteLine(run_cmd("python3 /Users/inderdeepbhatia/Projects/HTBTest1/HTBTest1", codeInput + " " + codeCSV));*/
+            //HTBTest1.SequenceMatcher sequenceMatcher = new HTBTest1.SequenceMatcher();
+
+            //var input = "Object obj = new Object();";
+
+            List<string> goodAnswers = new List<string>();
+            List<string> badAnswers = new List<string>();
+            List<string> extraData = new List<string>();
+
+            using (var reader = new StreamReader("/Users/inderdeepbhatia/Projects/HTBTest1/HTBTest1/data_please.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var data = csv.GetRecords<dynamic>().ToList();
+                //data.GetEnumerator().MoveNext();
+                //Console.WriteLine(data.ElementAt(0).QuestionBody);
+                double score = 0.0;
+                double total = 0.0;
+                for (int i = 0; i < data.Count; i++)
+                {
+                    var row = data.ElementAt(i);
+                    string hasCode = row.HasCode;
+                    double newScore = 0.0;
+                    if (hasCode == "True")
+                    {
+                        string answerBody = row.AnswerBody;
+                        string commentBody = row.CommentText;
+                        int answerScore = int.Parse(row.AnswerScore);
+                        double distance = (double)StringDistance.LevenshteinDistance(input, answerBody);
+                        double matchPercentage = (input.Length - (distance)) / input.Length;
+                        if (matchPercentage > 0.6)
+                        {
+                            newScore = getScore(answerBody, commentBody, matchPercentage, answerScore);
+                            total += matchPercentage * answerScore;
+                            if (newScore >= 0)
+                            {
+                                goodAnswers.Add(commentBody);
+                            }
+                            else
+                            {
+                                badAnswers.Add(commentBody);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        String questionBody = row.QuestionBody;
+                        String answerBody = row.AnswerBody;
+                        int answerScore = int.Parse(row.AnswerScore);
+                        double distance = (double)StringDistance.LevenshteinDistance(input, questionBody);
+                        double matchPercentage = (input.Length - distance) / input.Length;
+                        if (matchPercentage > 0.6)
+                        {
+                            newScore = getScore(questionBody, answerBody, matchPercentage, answerScore);
+                            total += matchPercentage * answerScore;
+                            if (newScore >= 0)
+                            {
+                                goodAnswers.Add(answerBody);
+                            }
+                            else
+                            {
+                                badAnswers.Add(answerBody);
+                            }
+                        }
+                    }
+                    score += newScore;
+                    //Console.WriteLine("Current Score: " + score);
+                }
+                extraData.Add(""+score);
+                extraData.Add("" +total);
+            }
+            return new List<string>[] { goodAnswers, badAnswers, extraData };
+
+            /*String answerBody = "Object obj = new Object();";
+            String commentBody = "That is acceptable code, but can use more memory than what you might be intending to";
+            double matchPercentage = 0.8;
+            int answerScore = 30;
+            WriteLine(getScore(answerBody, commentBody, matchPercentage, answerScore));*/
+
+        }
+
+        static String run_cmd(string cmd, string args)
+        {
+            ProcessStartInfo start = new ProcessStartInfo();
+            start.FileName = cmd;
+            start.Arguments = args;
+            start.UseShellExecute = false;
+            start.RedirectStandardOutput = true;
+            using (Process process = Process.Start(start))
+            {
+                using (StreamReader reader = process.StandardOutput)
+                {
+                    string result = reader.ReadToEnd();
+                    return result;
+                }
+            }
+        }
+
+        static double getScore(String answerBody, String commentBody, double matchPercentage, int answerScore)
+        {
+            if (answerScore == 0)
+            {
+                answerScore = 1;
+            }
+            commentBody = "{"
+             + "  \"document\": \"" + commentBody + "\""
+             + "}";
+            double commentSentiment;
+            if (getSentiment(commentBody).result.GetType().ToString().Equals("System.Int64"))
+            {
+                commentSentiment = 0.01;
+            }
+            else
+            {
+                //WriteLine(getSentiment(commentBody).result.GetType());
+                JArray result = (JArray)getSentiment(commentBody).result;
+                commentSentiment = (double)result[0]["sentiment"];
+            }
+            //WriteLine(commentSentiment);
+            var score = matchPercentage * answerScore * commentSentiment;
+
+            return score;
+        }
+
+        static AlgorithmResponse getSentiment(String input)
+        {
+            var client = new Client("sim+5Uw+MCHUKHUA9GZhJeOlihU1");
+            var algorithm = client.algo("nlp/SentimentAnalysis/1.0.5");
+            algorithm.setOptions(timeout: 300); // optional
+            var response = algorithm.pipeJson<object>(input);
+            return response;
+        }
+    }
+
+    public static class ShellHelper
+    {
+        public static string Bash(this string cmd)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return result;
+        }
+
+    }
+
+    public static class StringDistance
+    {
+
+        private static void Run()
+
+        {
+            //Console.WriteLine("Mama", "Mom");
+            var codeInput = "'Object obj = new Object();'";
+            var codeCSV = "'Object ob = new Object();'";
+            double distance = (double)StringDistance.LevenshteinDistance(codeInput, codeCSV);
+            //Console.WriteLine((codeInput.Length - (distance)) / codeInput.Length + "%");
+            //Console.WriteLine((6 - StringDistance.LevenshteinDistance("climax", "volmax")) / 100 + "%");
+            //Console.WriteLine(StringDistance.LevenshteinDistance("Ram", "Raman"));
+            //Console.WriteLine(StringDistance.LevenshteinDistance("Mama", "Mom"));
+
+
+        }
+
+
+        public static int LevenshteinDistance(string s, string t)
+        {
+
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            if (n == 0)
+            {
+                return m;
+            }
+
+            if (m == 0)
+            {
+                return n;
+            }
+
+            for (int i = 0; i <= n; d[i, 0] = i++)
+            {
+            }
+
+            for (int j = 0; j <= m; d[0, j] = j++)
+            {
+            }
+
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + cost);
+                }
+            }
+            return d[n, m];
+        }
+
+    }
+
+}
